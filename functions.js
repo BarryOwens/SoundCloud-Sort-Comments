5998b18ee305517c1ca5fd3b6d88c65721600bd2
Barry Owens <baz789@hotmail.com>
2015-05-16 00:43:30 +0100

    New Icon Changes

--- functions.js
+++ functions.js
@@ -1,33 +1,38 @@
-// initialize soundcloud API with key
+// Initialize SoundCloud API with key
 SC.initialize({
-    // This is the sample client_id. you should replace this with your own
     client_id: "02d54efe6695649f5c72cae57841e44a"
 });
-
+// Get the URL when on SoundCloud
 chrome.tabs.executeScript(null, {code: "var url = document.URL;url;"},
 		function(result){
-	// Do check for valid URL here
+	// Check that SoundCloud is being used
+	var soundcloudCheck=result[0].indexOf('soundcloud.com/');
+	if(soundcloudCheck<0){
+		// TODO Update some error text that URL is incorrect
+		console.log('Not SoundCloud URL');
+		return;
+	}
+	result[0].substring(soundcloudCheck+"soundcloud.com/".length,result[0].length)
 	var attributes=result[0].split('/');
 	var track=attributes[attributes.length-1];
 	var user=attributes[attributes.length-2];
+	// Create a PATH to be used to gather information about the track
 	var PATH = "/users/"+user+"/tracks/"+track;
-	console.log(PATH);
 	$(document).ready(
 	  function () {
-		// Use soundcloud API to get info of the track requested
+		// Use SoundCloud API to get info of the track requested
 		SC.get(PATH
 		  , function (track, err) {
 			// obtain track's id
 			var duration = track.duration;
-			var TRACK_ID = track.id;
-			SC.get("/tracks/"+TRACK_ID+"/comments"
+			var track_id = track.id;
+			SC.get("/tracks/"+track_id+"/comments"
 			  , function (comments, err) {
-			  // Construct html of output
 			  var output = "";
+			  // Sort the array by time occuring on track
 			  comments.sort(compare);
 			  for (var i = 0; i < comments.length; i++) {
-				  console.log(comments[i]);
-				output += "user: " + comments[i].user.username + "<br>";
+					output += "user: " + comments[i].user.username + "<br>";
 				output += "comment: " + comments[i].body + "   " +msToTime(comments[i].timestamp)  +" <hr> ";
 			  }
 			  $("#url").html(output);
@@ -35,6 +40,8 @@ chrome.tabs.executeScript(null, {code: "var url = document.URL;url;"},
 		  });
 	  });
 });
+
+// Compare function used to sort comments array by timestamp
 function compare(a,b) {
   if (a.timestamp < b.timestamp)
      return -1;
@@ -43,6 +50,7 @@ function compare(a,b) {
   return 0;
 }
 
+// Function to convert milliseconds into seconds minutes and hours.
 function msToTime(duration) {
     var milliseconds = parseInt((duration%1000)/100)
         , seconds = parseInt((duration/1000)%60)
